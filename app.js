cd8d5a3a4c4085aa32d4606dcdf93eb78b908a11
saikatmohanty753 <saikat.mohanty@oasystspl.com>
2023-08-07 10:37:51 +0530

    chenges in node

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import {getuser} from './database.js';
 dotenv.config();
 const app = express()
 
@@ -9,3 +10,12 @@ app.use(cors({origin:""}))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+app.listen(8080,function(req,res,next){console.log("listening on",8080)});
+
+app.get('/total', function(req, res){
+    if(getuser)
+    {
+        return res.json({"query":getuser[0]});
+    }
+    return res.json({"query":getuser});
+});
--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-import { Client, Pool } from "pg";
+import pg from "pg";
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,8 +10,15 @@ const credentials = {
     port:5432
 }
 
-const pool = new Pool(credentials);
-pool.query()
-const client = new Client(credentials);
-
-client.connect();
+const pool = new pg.Pool(credentials);
+pool.connect();
+export async function getuser(){
+    const [rows] = await pool.query('SELECT * FROM users').then((result)=>{
+        return result;
+    }).catch((err)=>{
+        console.log(err);
+        return;
+    });
+    console.log(rows,'called');
+    return ;
+}
